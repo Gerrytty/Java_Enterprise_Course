c8e70b1823ea6c229db48b82aaa063125f03f4a3
Yuliya Mihaylova <yulu.2000@hotmail.com>
2019-09-28 21:49:57 +0300

    program for dowloanding files

---
+++ HW_1/src/FileDowloandingThread.java
@@ -0,0 +1,85 @@
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+public class FileDowloandingThread extends Thread {
+    // colors
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_RED = "\u001B[31m";
+    private static final String ANSI_GREEN = "\u001B[32m";
+
+    private String pathFromDowloand;
+    private String pathToSave;
+
+    public FileDowloandingThread(String pathFromDowloand, String pathToSave) {
+        this.pathFromDowloand = pathFromDowloand;
+        this.pathToSave = pathToSave;
+    }
+
+    @Override
+    public void run() {
+        downloand(pathFromDowloand, pathToSave);
+    }
+
+
+    private void downloand(String sourceURL, String targetDirectory) {
+
+        try {
+            URL url = new URL(sourceURL);
+            String fileName = createFileName(sourceURL);
+            Path targetPath = new File(targetDirectory + File.separator + fileName).toPath();
+            Files.copy(url.openStream(), targetPath, StandardCopyOption.REPLACE_EXISTING);
+
+            System.out.println(ANSI_GREEN + "File from " + sourceURL + " saved to " + targetDirectory + " as "
+                    + fileName + ANSI_RESET);
+
+        } catch (IOException e) {
+            System.out.println(ANSI_RED + "Incorrect path " + sourceURL + " file not saved" + ANSI_RESET);
+        }
+
+    }
+
+
+    /***
+     * creating unique fileName
+     * if fileName is already exists return fileName_1
+     * if fileName_1 is already exists return fileName_2
+     ***/
+
+    private String createFileName(String sourceURL) {
+        String fileName = sourceURL.substring(sourceURL.lastIndexOf('/') + 1);
+        File file = new File(fileName);
+        String s;
+
+        if(file.exists()) {
+
+            String name = fileName.substring(0, fileName.indexOf("."));
+            String extension = fileName.substring(fileName.indexOf("."));
+            char c = name.charAt(name.length() - 1);
+
+            if(isNum(c)) {
+                int n = Integer.parseInt("" + c);
+                s = name.substring(0, name.lastIndexOf(String.valueOf(n))) + (n + 1) + extension;
+            }
+            else {
+                s = name + "_1" + extension;
+            }
+
+            return createFileName(s);
+
+        }
+
+        else {
+            return fileName;
+        }
+        
+    }
+
+    private boolean isNum(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+}
---
+++ HW_1/src/Main.java
@@ -0,0 +1,57 @@
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Main {
+    public static void main(String[] args) {
+
+        if (!netIsAvailable()) {
+            System.out.println("\u001B[31m" + "NO INTERNET" + "\u001B[0m");
+            System.exit(0);
+        }
+
+        System.out.println("\u001B[30m" + "Start dowloanding");
+
+        String pathToSaveFile = createFolder();
+
+        for (String arg : args) {
+            FileDowloandingThread thread = new FileDowloandingThread(arg, pathToSaveFile);
+            thread.run();
+        }
+
+    }
+
+    private static boolean netIsAvailable() {
+
+        try {
+            final URL url = new URL("http://www.google.com");
+            final URLConnection conn = url.openConnection();
+            conn.connect();
+            conn.getInputStream().close();
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+
+    }
+
+
+    /***
+     * creating folder "dowloands" in current directory for saving files
+     *
+     * @return path to this folder
+     ***/
+    private static String createFolder() {
+        String currentPath = System.getProperty("user.dir");
+        String pathToSaveFile = currentPath + "/dowloands";
+
+        File file = new File(pathToSaveFile);
+        if(!file.isDirectory()) {
+            file.mkdir();
+        }
+
+        return pathToSaveFile;
+    }
+
+}
